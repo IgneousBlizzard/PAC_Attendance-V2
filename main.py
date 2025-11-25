00f20f1a13e9e78a7308e817643e256183057dab
IgneousBlizzard <igneousblizzard@gmail.com>
2025-11-24 21:29:15 -0500

    fucking oop

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import json
+import datetime
 
+class staff:
+    staff_list = {}
+    def __init__(self, username, attendance=None, role=False): # Attendance = List of all recorded events attended | role = True (Pilot) / False (Crew Chief)
+        self.username = username
+        self.attendance = attendance if attendance is not None else []
+        self.role = role
+
+    def format_json():
+        return {
+            "attendance": self.attendance,
+            "role": self.role
+        }
+
+    @classmethod
+    def save_to_json(cls, filename):
+        pass
